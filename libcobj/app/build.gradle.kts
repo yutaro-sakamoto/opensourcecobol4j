bfdd214b09df242721176b2d863e914039a597fe
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-02-28 01:55:02 +0000

    chore: change the version number to 1.1.7

--- libcobj/app/build.gradle.kts
+++ libcobj/app/build.gradle.kts
@@ -80,7 +80,7 @@ publishing {
         register<MavenPublication>("gpr") {
             groupId = "jp.osscons.opensourcecobol"
             artifactId = "libcobj"
-            version = "1.1.6"
+            version = "1.1.7"
             from(components["java"])
         }
     }
