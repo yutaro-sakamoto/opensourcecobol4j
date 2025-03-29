fbdf68e4b58b9c2fdae01d40e20c8cd22bda2c9b
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-03-29 12:03:50 +0000

    doc: update version strings

--- libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/user_util/cobj_api/ApiFilesOptions.java
+++ libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/user_util/cobj_api/ApiFilesOptions.java
@@ -46,7 +46,7 @@ class ApiFilesOptions {
                             System.exit(0);
                             break;
                         case "v":
-                            System.out.println("1.1.7-hotfix1");
+                            System.out.println("1.1.8");
                             System.exit(0);
                             break;
                         case "java-package":
