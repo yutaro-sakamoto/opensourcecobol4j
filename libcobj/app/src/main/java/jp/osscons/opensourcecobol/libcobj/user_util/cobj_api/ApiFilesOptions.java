c83ffa5a66c2142f7ceace0ee8b81acbd490cefd
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-04-18 02:57:26 +0000

    build: update the version to 1.1.8-hotfix

--- libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/user_util/cobj_api/ApiFilesOptions.java
+++ libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/user_util/cobj_api/ApiFilesOptions.java
@@ -46,7 +46,7 @@ class ApiFilesOptions {
                             System.exit(0);
                             break;
                         case "v":
-                            System.out.println("1.1.8");
+                            System.out.println("1.1.8-hotfix1");
                             System.exit(0);
                             break;
                         case "java-package":
