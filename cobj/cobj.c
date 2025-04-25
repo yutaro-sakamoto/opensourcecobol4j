98338588e9e94e00d4366a8b698587fb339f7620
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-04-25 07:30:41 +0000

    chore: update the message of --version

--- cobj/cobj.c
+++ cobj/cobj.c
@@ -850,6 +850,11 @@ static void cobc_print_version(void) {
   printf("cobj (%s) %s\n", PACKAGE_NAME, PACKAGE_VERSION);
   puts("Copyright (C) 2021-2023 TOKYO SYSTEM HOUSE CO.,LTD.");
   printf("Built    %s\n", cb_oc_build_stamp);
+  puts("----");
+  puts("cobc (opensource COBOL) 1.5.2.0");
+  puts("----");
+  puts("cobc (OpenCOBOL) 1.1");
+  puts("Copyright (C) 2001-2009 Keisuke Nishida / Roger While");
 }
 
 static void cobc_print_usage(void) {
