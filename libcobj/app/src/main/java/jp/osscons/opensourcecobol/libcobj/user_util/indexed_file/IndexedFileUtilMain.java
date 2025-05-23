875a978217e5da62aa3845e177b491721e5af3e9
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-05-23 08:00:59 +0000

    refactor: convert a for loop to a for each loop

--- libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/user_util/indexed_file/IndexedFileUtilMain.java
+++ libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/user_util/indexed_file/IndexedFileUtilMain.java
@@ -282,8 +282,7 @@ class IndexedFileUtilMain {
                 matcher = patternContains0.matcher(keyOption);
                 if (matcher.matches()) {
                     String[] keys = keyOption.split(":");
-                    for (int i = 0; i < keys.length; i++) {
-                        String key = keys[i];
+                    for (String key : keys) {
                         String[] keyInfo = key.split(",");
                         String offsetString = keyInfo[0];
                         if (offsetString.startsWith("d")) {
