b5e51220788a75e13ff023da4c3fcb89cc9a4396
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-06-30 01:21:13 +0000

    chore: update version strings

--- libcobj/app/build.gradle.kts
+++ libcobj/app/build.gradle.kts
@@ -80,7 +80,7 @@ publishing {
         register<MavenPublication>("gpr") {
             groupId = "jp.osscons.opensourcecobol"
             artifactId = "libcobj"
-            version = "1.1.9"
+            version = "1.1.10"
             from(components["java"])
         }
     }
