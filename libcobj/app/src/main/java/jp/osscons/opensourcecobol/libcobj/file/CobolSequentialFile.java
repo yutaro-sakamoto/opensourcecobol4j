1b8b4a143c57c5f45773907463c83bb275073c51
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-09-22 00:59:43 +0000

    fix: check that the record size is not less than record_min

--- libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/file/CobolSequentialFile.java
+++ libcobj/app/src/main/java/jp/osscons/opensourcecobol/libcobj/file/CobolSequentialFile.java
@@ -135,7 +135,9 @@ public class CobolSequentialFile extends CobolFile {
                 }
             }
             int size = ByteBuffer.wrap(sbuff).getInt();
-            this.record.setSize(Math.min(size, this.record_max));
+            size = Math.max(size, this.record_min);
+            size = Math.min(size, this.record_max);
+            this.record.setSize(size);
         }
 
         try {
