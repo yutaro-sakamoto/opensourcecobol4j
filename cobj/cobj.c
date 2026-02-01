e7d8a86fc3eee85765017dcc14026da2ca93754d
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-02-01 23:16:33 +0900

    refactor: format

--- cobj/cobj.c
+++ cobj/cobj.c
@@ -1909,8 +1909,8 @@ static int process_compile_all(void) {
 #else
       char remove_cmd[] = "rm";
 #endif
-      snprintf(buff, BUFF_SIZE, "%s %s%c%s%c%s.class",
-               remove_cmd, output_name_a, file_path_delimitor, package_dir,
+      snprintf(buff, BUFF_SIZE, "%s %s%c%s%c%s.class", remove_cmd,
+               output_name_a, file_path_delimitor, package_dir,
                file_path_delimitor, *program_id);
       process(buff);
     }
